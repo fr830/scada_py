0b15bf7513a2a8d945e645e9eec96a9ad655d9a5
Matthew Farmer <mjfarmer@uno.edu>
2016-04-18 13:04:28 -0500

    working comms with other pi and timing

--- simpleTest.py
+++ simpleTest.py
@@ -1,7 +1,13 @@
 from pymodbus.client.sync import ModbusTcpClient
+import timeit
 
-client = ModbusTcpClient('127.0.0.1')
-client.write_coil(1, True)
-result = client.read_coils(1,1)
-print result.bits[0]
+client = ModbusTcpClient('127.0.0.1', 5020)
+client.connect()
+def write(c):
+   c.write_coil(1, False)
+   c.read_coils(1,1)
+print timeit.timeit(lambda:write(client), number=100)
+
+#result = client.read_coils(1,1)
+#print result.bits[0]
 client.close()
